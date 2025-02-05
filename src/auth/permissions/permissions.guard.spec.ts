cf2cee6e0ba7f208b970c87661ba979e5879ce11
Florian Bieck <info@florianbieck.com>
2025-02-05 17:27:45 +0100

    Refactor test cases for consistency and dependency setup.
    Updated test files to improve naming consistency (e.g., 'products' replaced with 'models') and ensure proper dependency initialization by importing necessary modules like HttpModule and ConfigModule. Adjusted PermissionsGuard test to use Reflector for better accuracy.

--- src/auth/permissions/permissions.guard.spec.ts
+++ src/auth/permissions/permissions.guard.spec.ts
@@ -1,7 +1,20 @@
 import { PermissionsGuard } from './permissions.guard';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Reflector } from '@nestjs/core';
 
 describe('PermissionsGuard', () => {
+  let reflector: Reflector;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
+      controllers: [],
+    }).compile();
+
+    reflector = module.get<Reflector>(Reflector);
+  });
+
   it('should be defined', () => {
-    expect(new PermissionsGuard()).toBeDefined();
+    expect(new PermissionsGuard(reflector)).toBeDefined();
   });
 });
--- src/keycloak-resources/infrastructure/keycloak-resources.service.spec.ts
+++ src/keycloak-resources/infrastructure/keycloak-resources.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { KeycloakResourcesService } from './keycloak-resources.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('KeycloakResourcesService', () => {
   let service: KeycloakResourcesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule],
       providers: [KeycloakResourcesService],
     }).compile();
 
