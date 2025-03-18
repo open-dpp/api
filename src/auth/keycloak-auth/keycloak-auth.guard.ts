471c86e9ab1da0ade67091665e253ec9e0262f85
Florian Bieck <info@florianbieck.com>
2025-03-18 14:40:28 +0100

    Use dynamic public key from config in KeycloakAuthGuard
    Replaced the hardcoded JWT public key with a dynamically fetched value from the configuration service. This improves flexibility and allows updates to the key without modifying the codebase.

--- src/auth/keycloak-auth/keycloak-auth.guard.ts
+++ src/auth/keycloak-auth/keycloak-auth.guard.ts
@@ -18,9 +18,6 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class KeycloakAuthGuard implements CanActivate {
-  private readonly PUBLIC_KEY =
-    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtOVDaYfkFeq2eQvx5IcT04MPTGq8iPM0iEf+9LJ9YYkPR2BmEodqDmV9p6XgkHg0/JoRI8JPZVKe9wTmVO+KEvwQF/U9uaCSe0xVfaYv3YkaxsKzoKEfXNluFgQG0jm+NRzUdeUj8MbHUAbOkaS+UBgrqF8pASxdyqjBJ5kcQsZ5KeXj1eUmUPVMwcnoXju+e1h7a1ql0vyUlkrH6pkMStglkkXNYtaTa8dByO7+LOQzoTEzECsLFt5p0WTLMxR1Lutr//nK+ELMN562gUOlipbBjS420YWWW184Bnx3mK2mg2Ldn4GHBex9mjDb5ECEc/eNylgVwwaNinJ2T/WhIQIDAQAB';
-
   constructor(
     private reflector: Reflector,
     private httpService: HttpService,
@@ -61,19 +58,18 @@ export class KeycloakAuthGuard implements CanActivate {
     const accessToken = parts[1];
 
     const authContext = new AuthContext();
-    let keycloakId = null;
     authContext.permissions = [];
 
     const payload = await this.jwtService.verifyAsync(accessToken, {
       algorithms: ['RS256'],
-      publicKey: this.formatPublicKey(this.PUBLIC_KEY),
+      publicKey: this.formatPublicKey(
+        this.configService.get('KEYCLOAK_JWT_PUBLIC_KEY'),
+      ),
     });
-    keycloakId = payload.sub;
     const user: KeycloakUserInToken = payload;
-    keycloakId = user.sub;
     authContext.keycloakUser = user;
     await this.usersService.create(user, true);
-    authContext.user = new User(keycloakId, user.email);
+    authContext.user = new User(payload.sub, user.email);
     const memberships = payload.memberships || ([] as string[]);
     memberships.forEach((membership: string) => {
       authContext.permissions.push({
