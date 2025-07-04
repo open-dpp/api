aef33c275b4ab10be0c4b6038d621afbaf152ea5
mrudolph <michael.rudolph@open-dpp.de>
2025-07-04 15:03:05 +0200

    refactor: Refine KeycloakAuthGuard and replace HttpException with UnauthorizedException
    Replaced `HttpException` with `UnauthorizedException` for better exception handling. Extracted URL generation logic into a dedicated method for improved readability. Adjusted related tests and error messaging accordingly.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
     }),
-    HttpModule,
   ],
   providers: [],
   exports: [],
